6f70daaea8aa2ff15a4250c039da5e00ef53d6a1
Tomasz Ceremuga <84967908+tomaszceremuga@users.noreply.github.com>
2023-03-24 14:50:32 +0100

    2

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 let amount = 0
 const body = document.querySelector('body')
+let move = false
 
-
-
-document.addEventListener('click', (e) => {
+const createBox = (e) => {
 
     const box = document.createElement("div")
+
     box.setAttribute('class', 'box')
     box.style.left = `${e.pageX - 103}px`
     box.style.top = `${e.pageY - 103}px`
     box.textContent = amount
+    box.style.background = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`
+
     document.body.appendChild(box)
     amount++
     box.addEventListener('contextmenu', (e) => {
@@ -17,5 +19,23 @@ document.addEventListener('click', (e) => {
         amount--
         e.preventDefault()
     })
+    box.addEventListener("mousemove", (e) => {
+        document.removeEventListener("click", createBox);
+        if (move) {
+            box.style.top = (e.clientY - 103) + "px"
+            box.style.left = (e.clientX - 103) + "px"
+        }
+        box.addEventListener("mousedown", () => {
+            move = true
+        })
+        box.addEventListener("mouseup", () => {
+            move = false
+        })
+        // document.addEventListener('click', createBox)
 
-})
+        // cza dodac listenera po poruszeniu
+    })
+
+}
+
+document.addEventListener('click', createBox)
